5182c27d09a95cf442f101a31273d92f1b9b152b
Shreesha Aatreya <shreeshaaatreya1@gmail.com>
2024-03-27 10:02:28 +0545

    Some Content Added

--- my-portfolio/src/components/project/ProjectIcon.jsx
+++ my-portfolio/src/components/project/ProjectIcon.jsx
@@ -1,29 +1,38 @@
-import React ,{Component,Fragment} from "react";
+import React, { useState, useEffect } from "react";
 import Fade from 'react-reveal/Fade';
-
 import hms from '../images/hospital.png';
 import PT from '../images/productivity.jpg';
 import DS from '../images/sambidhan.jpg';
 import SR from '../images/sastodeal.png';
 import DMS from '../images/donation.jpg';
 
-function ProjectIcon(){
-    return(
-        <div className="flex-col  mt-10">
-            <div className="flex justify-center items-center mt-5 ">
-                <img src={hms} alt="hospital Management Systems Icon" className="rounded-full w-32 h-32 object-cover shadow-md shadow-cyan-300/100" />
-            </div>
+function ProjectIcon() {
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const animateList = [hms, PT, DS, SR, DMS];
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCurrentIndex(prevIndex => (prevIndex + 1) % animateList.length);
+        }, 5000); 
+        return () => clearInterval(interval);
+    }, [animateList.length]);
+
+    const displayIndex = (index) => {
+        return (index + currentIndex) % animateList.length;
+    };
+
+    return (
+        <div className="flex-col mt-10">
             <div className="flex justify-center items-center mt-5">
-            <img src={PT} alt="Productivity Tracker Icon" className="rounded-full w-32 h-32 object-cover shadow-md shadow-cyan-300/100" />
-            </div>
-            <div className="flex justify-center items-center mt-5">
-            <img src={DS} alt="Productivity Tracker Icon" className="rounded-full w-32 h-32 object-cover shadow-md shadow-cyan-300/100" />
-            </div>
-            <div className="flex justify-center items-center mt-5">
-            <img src={SR} alt="Productivity Tracker Icon" className="rounded-full w-32 h-32 object-cover shadow-md shadow-cyan-300/100" />
-            </div>
-            <div className="flex justify-center items-center mt-5">
-            <img src={DMS} alt="Productivity Tracker Icon" className="rounded-full w-32 h-32 object-cover shadow-md shadow-cyan-300/100" />
+                <Fade>
+                    <div className="flex">
+                        {[0, 1].map((index) => (
+                            <div key={index} className="mr-5">
+                                <img src={animateList[displayIndex(index)]} alt={`Project Icon ${displayIndex(index)}`} className="rounded-full w-32 h-32 object-cover shadow-md shadow-cyan-300/100" />
+                            </div>
+                        ))}
+                    </div>
+                </Fade>
             </div>
         </div>
     )
